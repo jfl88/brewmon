907ad5e878e63e4f1488b3b2074d62b9738be4ab
Jason Lyons <butters1@gmail.com>
2017-07-06 20:36:53 -0700

    Try switch to Plot.ly

--- public/js/brewtest.js
+++ public/js/brewtest.js
@@ -5,88 +5,28 @@
         .module('brewtest', ['nvd3'])
         .controller('homeCtrl', ['$scope','$interval', '$http',
         function ($scope, $interval, $http) {
-            $scope.friend = 'alfred';
-
-            $scope.chartOptions = {
-                chart: {
-                    type: "lineWithFocusChart",
-                    height: 450,
-                    margin: {
-                    top: 20,
-                    right: 20,
-                    bottom: 40,
-                    left: 55
-                    },
-                    useInteractiveGuideline: true,
-                    dispatch: {},
-                    xAxis: {
-                        tickFormat: function(d) {
-                            return d3.time.format('%H:%M:%S')(new Date(d));
-                        },
-                        axisLabel: "Timestamp"
-                    },
-                    yAxis: {
-                        axisLabel: "Temperature (°C)",
-                        axisLabelDistance: -10
-                    },
-                    x2Axis: {
-                        tickFormat: function(d) {
-                            return d3.time.format('%d-%m-%Y')(new Date(d));
-                        },
-                        showMaxMin: false,
-                        "axisLabel": null,
-                        "height": 60,
-                        "ticks": null,
-                        "width": 75,
-                        "margin": {
-                            "top": 0,
-                            "right": 0,
-                            "bottom": 0,
-                            "left": 0
-                        },
-                        "duration": 250,
-                        "orient": "bottom",
-                        "tickValues": null,
-                        "tickSubdivide": 0,
-                        "tickSize": 6,
-                        "tickPadding": 5,
-                        "domain": [
-                            0,
-                            1
-                        ],
-                        "range": [
-                            0,
-                            1
-                        ]   
-                    }
-                },
-                title: {
-                    enable: true,
-                    text: "Brew Temperature"
-                }
-            };
-
             $http.get('/api/brews').then(function success(resp) {
                 $scope.currentBrew = resp.data[0];
-
-                $scope.chartData = [
-                    {
-                        values: [{
-                            x: new Date($scope.currentBrew.tempData[0].timestamp),
-                            y: $scope.currentBrew.tempData[0].temp
-                        }],
-                        key: 'Temp'
-                    }
-                ];
                 
+                $scope.brewData = [{
+                    x: [],
+                    y: [],
+                    type: 'scatter'
+                }];
+
+                var layout = {
+                    title: 'Brew Temperature',
+                    showlegend: false,
+                    xaxis: { title: 'Date / Time' },
+                    yaxis: { title: 'Temperature (°C)' }
+                };
+
                 $scope.currentBrew.tempData.forEach(function(record) {
-                    $scope.chartData[0].values.push({
-                        x: new Date(record.timestamp),
-                        y: record.temp
-                    });
+                    $scope.brewData[0].x.push(new Date(record.timestamp));
+                    $scope.brewData[0].y.push(record.temp)
                 });
 
-                console.log('min' + d3.min($scope.chartData[0].values, function (d){ return d.y; }));
+                Plotly.newPlot('brewGraph', $scope.brewData, layout, { displaylogo: false });
             });
 
           $scope.liveTemp = 0.0;
@@ -101,7 +41,6 @@
               $scope.liveTemp = data.temp;
             });
           });
-
         }
     ]);
 })();
