367bf5faec8f1acc72d02a237be805ed8c572e05
Jason Lyons <butters1@gmail.com>
2017-09-23 19:21:36 -0700

    created edit page, updated http authentication to use non-published credentials

--- routes/control.js
+++ routes/control.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var basicAuth = require('basic-auth');
 
+// db stuff
+var dblogin = require('../dblogin.json');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var assert = require('assert');
+var url = 'mongodb://' + dblogin.user + ':' + dblogin.pw + '@' + dblogin.addr;
+
 var auth = function (req, res, next) {
   function unauthorized(res) {
     res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
@@ -10,22 +17,19 @@ var auth = function (req, res, next) {
 
   var user = basicAuth(req);
 
+  console.log(dblogin);
+  console.log(user);
+  
   if (!user || !user.name || !user.pass) {
     return unauthorized(res);
   };
 
-  if (user.name === 'foo' && user.pass === 'bar') {
+  if (user.name === dblogin.control_login && user.pass === dblogin.control_password) {
     return next();
   } else {
     return unauthorized(res);
   };
 };
-
-// db stuff
-var dblogin = require('../dblogin.json');
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var url = 'mongodb://' + dblogin.user + ':' + dblogin.pw + '@' + dblogin.addr;
 
 /* GET list page. */
 router.get('/brewlist', auth, function(req, res, next) {
@@ -35,25 +39,25 @@ router.get('/brewlist', auth, function(req, res, next) {
     .sort({ startDT: -1 })
     .toArray(function(err, docs) {
       assert.equal(err, null);
-      console.log("Found the following records");
-      console.dir(docs)
-      res.render('brewlist', { title: 'Jason\'s Magical Brewing Land - Control Centre', brews: docs });
+      res.render('brewlist', { title: 'Jason\'s Magical Brewing Land - Brewing Control Centre', brews: docs });
     });      
   });
 });
 
 router.get('/brew/:brewid', auth, function(req, res, next) {
+  console.log(req.params.brewid);
   MongoClient.connect(url, function(err, db){
     db.collection('brews')
-    .find(req.query)
-    .sort({ startDT: -1 })
-    .toArray(function(err, docs) {
+    .findOne({ "_id": ObjectId(req.params.brewid)}, function(err, doc) {
       assert.equal(err, null);
-      console.log("Found the following records");
-      console.dir(docs)
-      res.render('brewlist', { title: 'Jason\'s Magical Brewing Land - Control Centre', brews: docs });
+      res.render('editbrew', { title: 'Jason\'s Magical Brewing Land - Brewing Control Centre', brew: doc });
     });      
   });
+});
+
+router.post('/brew/:brewid', auth, function(req, res, next) {
+  console.log(req.body);
+  res.send('Post page');
 });
 
 module.exports = router;
